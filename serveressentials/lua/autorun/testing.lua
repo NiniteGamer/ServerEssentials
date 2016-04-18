7d4b2b34352ff2f8610b8cbb4722073391f8457c
Josh Bennett <theninitegamer@gmail.com>
2016-04-18 13:39:01 -0400

    Delete testing.lua

--- serveressentials/lua/autorun/testing.lua
+++
@@ -1,8 +0,0 @@
-local exolib = include("exolib.lua")
-
-local BannedWords = {"Test","Testing","TestingBigTime"}
-
-function Koolio( ply )
-	PrintTable(BannedWords)
-end
-hook.Add("PlayerSay", "Koolio", Koolio)
